54fb1b2efabf0320af579b739d155b6a6c423c9e
Mike Vercoelen <mike.vercoelen@gmail.com>
2018-04-04 14:39:38 +0200

    Update 2018-04-04T12:39:36.452Z

--- README.md
+++
@@ -1,3 +0,0 @@
-# React Modal Construction Kit
-
-Flexible, scalable library for creating your modal components with React.
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import Modal from './Modal/Modal'
-import Overlay from './Overlay/Overlay'
-import Portal from './Portal/Portal'
-
-export {
-  Modal,
-  Overlay,
-  Portal
-}
